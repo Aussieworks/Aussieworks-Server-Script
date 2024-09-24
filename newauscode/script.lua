64ea5e175854e08597a8906e51585555f0de1f17
ChickenMst <mgeek602@gmail.com>
2024-09-25 09:15:58 +1000

    Update script.lua

--- newauscode/script.lua
+++ newauscode/script.lua
@@ -8,8 +8,8 @@ PermMod = 2
 PermAdmin = 3
 PermOwner = 4
 
--- admin list
-adminlist = {{"76561199240115313",PermOwner},{"76561199143631975",PermAdmin},{"76561199032157360",PermAdmin},{"76561198371768441",PermAdmin}}
+-- admin list. formating: adminlist = {{{"76561199240115313",PermOwner},{"76561199143631975",PermAdmin}}
+adminlist = {}
 
 -- list that doesnt save
 nosave = {playerdata={}}
