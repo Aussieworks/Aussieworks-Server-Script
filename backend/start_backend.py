c296cc9276c9c9994da4d915661314829590590f
ChickenMst <mgeek602@gmail.com>
2025-05-20 17:31:23 +1000

    testing to see what causes webhook to stop working

--- backend/start_backend.py
+++ backend/start_backend.py
@@ -22,8 +22,6 @@ async def lifespan(app: FastAPI):
     task1 = asyncio.create_task(update_discord_webhook())
     task2 = asyncio.create_task(purge_stale_servers())
     yield
-    task1.cancel()
-    task2.cancel()
 
 app = FastAPI(lifespan=lifespan)
 
